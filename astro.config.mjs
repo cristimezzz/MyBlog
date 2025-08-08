0e82150a81644934e033afb551be79337ef5669c
Cristimezzz <thecristime@outlook.com>
2025-08-08 21:48:25 +0800

    更新基础路径配置，将博客的基础路径更改为 /MyBlog/

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://cristimezzz.github.io/",
-	base: "/",
+	base: "/MyBlog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
